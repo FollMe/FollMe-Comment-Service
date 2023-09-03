dc2af77948e39fe515c1075ed8f4e21cee1b09d5
sumgu <sum.dv@geekup.vn>
2023-09-04 01:17:59 +0700

    Public some routes

--- pkg/server/route.go
+++ pkg/server/route.go
@@ -11,11 +11,14 @@ func Route() {
 	app := NewApp()
 
 	router := mux.NewRouter().StrictSlash(true)
+	protectedRouter := router.NewRoute().Subrouter()
 	const BaseUrl = "/comment-svc/api"
-	router.Use(middleware.AuthenticationMiddleware)
+
 	router.Use(middleware.FilterPanicMiddleware)
+	protectedRouter.Use(middleware.AuthenticationMiddleware)
+
 	router.HandleFunc(BaseUrl+"/comments/{postId}", app.CmtHandler.GetCommentsOfPost).Methods("GET")
-	router.HandleFunc(BaseUrl+"/comments", app.CmtHandler.CreateCommentsOfPost).Methods("POST")
+	protectedRouter.HandleFunc(BaseUrl+"/comments", app.CmtHandler.CreateCommentsOfPost).Methods("POST")
 	router.HandleFunc(BaseUrl+"/comments/count", app.CmtHandler.GetNumberCommentsOfPosts).Methods("POST")
 	router.HandleFunc("/comment-svc/ws", app.CmtHandler.StartWSConnection).Methods("GET")
 
