7a2e97ce1850adc0e0925bb83c3f4e2b96e771c0
sumgu <sum.dv@geekup.vn>
2023-10-20 22:41:39 +0700

    Enhance log

--- pkg/middleware/filter_panic_middleware.go
+++ pkg/middleware/filter_panic_middleware.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"encoding/json"
 	"follme/comment-service/pkg/adapter/serializer"
+	"log"
 
 	"net/http"
 )
@@ -11,6 +12,7 @@ func FilterPanicMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		defer func() {
 			if r := recover(); r != nil {
+				log.Printf("Panic: %v", r)
 				w.WriteHeader(http.StatusInternalServerError)
 				json.NewEncoder(w).Encode(
 					serializer.NewFailHttpRes(""),
